e38a72411bca8daa1bff822bc86fdb91823059ff
liujiaodaliao <138237502+liujiaodaliao@users.noreply.github.com>
2024-04-10 10:42:15 +0200

    Update accountService.js

--- server/services/accountService.js
+++ server/services/accountService.js
@@ -7,19 +7,19 @@ const createAccount = (userId) => [
   {
     userId,
     accountTitle: "Argent Bank Checking (x8349)",
-    amount: 0,
+    amount: 2082.79,
     description: "Available Balance",
   },
   {
     userId,
     accountTitle: "Argent Bank Savings (x6712)",
-    amount: 0,
+    amount: 10928.42,
     description: "Available Balance",
   },
   {
     userId,
     accountTitle: "Argent Bank Credit Card (x8349)",
-    amount: 0,
+    amount: 184.3,
     description: "Current Balance",
   },
 ];
